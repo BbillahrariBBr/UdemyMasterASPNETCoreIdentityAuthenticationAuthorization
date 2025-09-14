e4883cec38926b119476e229e516806a3f9ea50c
BbillahrariBBr <mb.billah8@gmail.com>
2025-09-14 17:05:32 +0600

    Create Login Page 1.7 Done

---
+++ WebApp_Security/WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; } = new Credential();
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Description = "User Name")]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
